b5c230fd97972b11901a74b41965599c961f975a
Purushotam Kumar <kumarsiddy@gmail.com>
2020-07-15 08:50:56 +0000

    Feature/project setup (#2)
    * setup done with all the necessary basic classes

---
+++ moneybot/lib/src/customview/loader.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:moneybot/src/customview/text.dart';
+import 'package:moneybot/src/resources/colors.dart';
+import 'package:moneybot/src/resources/strings.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+class Loader {
+  static ProgressDialog getInstance(
+    BuildContext context, {
+    isDismissible = false,
+    message = AppString.loading,
+  }) {
+    return new ProgressDialog(
+      context,
+      type: ProgressDialogType.Normal,
+      isDismissible: isDismissible,
+      customBody: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Container(
+          height: 64,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              CircularProgressIndicator(
+                strokeWidth: 2.5,
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    AppColor.PRIMARY.color),
+                backgroundColor: Colors.white,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: SubTitleText(
+                  message,
+                  color: AppColor.PRIMARY.color,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ moneybot/lib/src/ui/sign_in.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInState();
+  }
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {}
+}
